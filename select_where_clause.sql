68ff53e83ced838133ee7ba4ef6e29913b6c980a
adithyaaneesh <adithyaaneesh29@gmail.com>
2025-10-30 11:35:36 +0530

    fix date issue

---
+++ select_where_clause.sql
@@ -0,0 +1,7 @@
+select emp_name, salary from employees;
+select emp_name, salary from employees where salary > 60000;
+SELECT * FROM employees WHERE join_date > '2021-01-01';
+select * from employees where dept_id = 2;
+select emp_name from employees where emp_name like 'A%';
+
+
